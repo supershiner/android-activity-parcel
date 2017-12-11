96f95c29bfc4f2b4609951aa92c989790428c4f9
supershiner <34336538+supershiner@users.noreply.github.com>
2017-12-12 02:42:28 +0800

    update

--- MyPicker/app/src/main/java/com/example/jlwj/picker/OnDateTimeChangeListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.jlwj.picker;
-
-/**
- *
- */
-
-public interface OnDateTimeChangeListener {
-    public void onDateTimeChange(DateTimePicker view, int year, int monthOfYear, int dayOfMonth, int hour, int minute);
-}
